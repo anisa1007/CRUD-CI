1396b02aea7244535c2e72b2d861c56fd65afd86
anisa1007 <anisaraden25@gmail.com>
2020-11-14 00:19:47 -0600

    Pratikum ke-6 Koneksi Database

---
+++ app/Controllers/Produk.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Controllers;
+
+use CodeIgniter;
+use App\Models\ProdukModel;
+
+class Produk extends BaseController
+{
+    public function index()
+    {
+        $model = new ProdukModel();
+        $data['produk'] = $model->getProduk();
+
+        echo view('produk\index', $data);
+    }
+}
